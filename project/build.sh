ccae1a415984ae8c9127c7b55e8fda8432d80237
JBruno <jbrunoealves@gmail.com>
2026-01-06 12:10:35 -0300

    primeiro commit

---
+++ project/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# bear -- \
+# 	gcc src/*.c -I include -Wall -Wextra -o game \
+# 		-lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+
+if [ $1 -eq 1 ]; then
+	bear -- \
+		gcc src/*.c -I include -Wall -Wextra -ggdb3 -fsanitize=address,undefined -o bin/game \
+			-lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+else
+	bear -- \
+		gcc src/*.c -I include -Wall -Wextra -fsanitize=address,undefined -o bin/game \
+			-lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+fi
